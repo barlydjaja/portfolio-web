4f1d842719420509304668f1ef365e81eb01daee
barly.djaja <barly.djaja@rupiahcepat.co.id>
2022-01-24 17:57:50 +0700

    fix apostrophe

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,8 +6,8 @@ const index = () => {
     return (
         <div className={'flex flex-col px-6 pt-1 flex-grow'}>
             <h5 className={'my-3 font-medium'}>
-                I'm a FullStack Developer. I have been working for more than 2 years now in Frontend
-                development. I'm using ReactJs and VueJs on daily basis at work. This website is created to show
+                I&apos;m a FullStack Developer. I have been working for more than 2 years now in Frontend
+                development. I&apos;m using ReactJs and VueJs on daily basis at work. This website is created to show
                 people my personal portfolio. I hope you can enjoy it!
             </h5>
             <div className={'p-4 mt-5 bg-gray-400 flex-grow'} style={{marginLeft: '-1.5rem', marginRight: '-1.5rem'}}>
